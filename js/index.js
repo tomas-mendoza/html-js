8e698365b8f8ac9a19f7ec54eacfaae3d49e1b8e
Tomás <tomas153lm@gmail.com>
2024-04-18 09:03:15 -0300

    feat: add html and js

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function createUser() {
+  document.querySelector('.name').innerHTML = document.getElementById('name').value;
+  document.querySelector('.email').innerHTML = document.getElementById('email').value;
+  document.querySelector('.password').innerHTML = document.getElementById('password').value;
+  document.querySelector('.date').innerHTML = document.getElementById('date').value;
+}
